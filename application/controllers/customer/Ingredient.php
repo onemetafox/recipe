3d07a68a4241d4040413f13daa6649a5f9c760e7
Dante Ika <danteikabuz@gmail.com>
2021-04-14 22:39:31 +0200

    update open food fact function

--- application/controllers/customer/Ingredient.php
+++ application/controllers/customer/Ingredient.php
@@ -26,56 +26,60 @@ class Ingredient extends CustomerController {
 	 */
 	public function api(){
 		$filter = $this->input->post();
-		if(isset($filter["query"]["search"])){
-			$url = "https://en.openfoodfacts.org/ingredient/". $filter["query"]["search"] . ".json";
+		$api = new OpenFoodFacts\Api('food','fr');
+		// $product = $api->getProduct('3057640385148');
+		// print_r($product);
+		$ingredient = $api->getIngredients("bread", "allergen");
+		// if(isset($filter["query"]["search"])){
+		// 	$url = "https://en.openfoodfacts.org/ingredient/". $filter["query"]["search"] . ".json";
 
-			$curlSession = curl_init();
-			curl_setopt($curlSession, CURLOPT_URL, $url);
-			curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-			curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-			$ingredients = json_decode(curl_exec($curlSession));
-			curl_close($curlSession);
-			// if(isset($filter["pagination"])){
-			// 	$pagination = $filter["pagination"];
-			// 	if(!$pagination["perpage"]){
-			// 		$pagination["perpage"] = 24;
-			// 	}
-			// 	if(!$pagination["page"]){
-			// 		$pagination["page"] = 1;
-			// 	}
-			// 	$pagination["total"] = $ingredients->count;
-			// 	$pagination["pages"] = ceil($pagination["total"]/$pagination["perpage"]);
+		// 	$curlSession = curl_init();
+		// 	curl_setopt($curlSession, CURLOPT_URL, $url);
+		// 	curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+		// 	curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+		// 	$ingredients = json_decode(curl_exec($curlSession));
+		// 	curl_close($curlSession);
+		// 	// if(isset($filter["pagination"])){
+		// 	// 	$pagination = $filter["pagination"];
+		// 	// 	if(!$pagination["perpage"]){
+		// 	// 		$pagination["perpage"] = 24;
+		// 	// 	}
+		// 	// 	if(!$pagination["page"]){
+		// 	// 		$pagination["page"] = 1;
+		// 	// 	}
+		// 	// 	$pagination["total"] = $ingredients->count;
+		// 	// 	$pagination["pages"] = ceil($pagination["total"]/$pagination["perpage"]);
 
-			// 	$limit["start"] = ($pagination["page"]-1) * $pagination["perpage"];
-			// 	$limit["end"] = $pagination["perpage"];
-			// 	$data["pagination"] = $pagination;
-			// }
+		// 	// 	$limit["start"] = ($pagination["page"]-1) * $pagination["perpage"];
+		// 	// 	$limit["end"] = $pagination["perpage"];
+		// 	// 	$data["pagination"] = $pagination;
+		// 	// }
 
-			$result = array();
-			// for($i = $limit["start"]; $i < $limit["end"]; $i++){
-			$products = $ingredients->products;
-			for($i = 0; $i < 24; $i++){
-				if(isset($products[$i]->brands)){
-					$temp["name"] = $products[$i]->brands;
-					$temp["type"] = "raw";
-					$temp["img"] = array();
-					$temp["allergen"] = $products[$i]->allergens;
-					$allergen_tags = $products[$i]->allergens_tags;
+		// 	$result = array();
+		// 	// for($i = $limit["start"]; $i < $limit["end"]; $i++){
+		// 	$products = $ingredients->products;
+		// 	for($i = 0; $i < 24; $i++){
+		// 		if(isset($products[$i]->brands)){
+		// 			$temp["name"] = $products[$i]->brands;
+		// 			$temp["type"] = "raw";
+		// 			$temp["img"] = array();
+		// 			$temp["allergen"] = $products[$i]->allergens;
+		// 			$allergen_tags = $products[$i]->allergens_tags;
 					
-					foreach($allergen_tags as $tag){
-						$aller = explode(":", $tag);
-						$image = $this->allergen->getOneByParam(array("name"=>$aller[1]));
-						if($image)
-							array_push($temp["img"], $image["img"]);
-					}
-					array_push($result, $temp);
-				}
+		// 			foreach($allergen_tags as $tag){
+		// 				$aller = explode(":", $tag);
+		// 				$image = $this->allergen->getOneByParam(array("name"=>$aller[1]));
+		// 				if($image)
+		// 					array_push($temp["img"], $image["img"]);
+		// 			}
+		// 			array_push($result, $temp);
+		// 		}
 				
 				
-			}
-			$data["data"] = $result;
-			// $data["meta"] = $pagination;
-			$this->json($data);
-		}
+		// 	}
+		// 	$data["data"] = $result;
+		// 	// $data["meta"] = $pagination;
+		// 	$this->json($data);
+		// }
 	}
 }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,128 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '536793f769fb4f72b544fe7b4100301019c7d2ad',
+    'name' => 'codeigniter/framework',
+  ),
+  'versions' => 
+  array (
+    'codeigniter/framework' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '536793f769fb4f72b544fe7b4100301019c7d2ad',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.3.0',
+      'version' => '7.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7008573787b430c1c1f650e3722d9bba59967628',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.4.1',
+      'version' => '1.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8e7d04f1f6450fef59366c399cfad4b9383aa30d',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.8.1',
+      'version' => '1.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '35ea11d335fd638b5882ff1725228b3d35496ab1',
+    ),
+    'mikey179/vfsstream' => 
+    array (
+      'pretty_version' => 'v1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc0fe8f4d0b527254a2dc45f0c265567c881d07e',
+    ),
+    'openfoodfacts/openfoodfacts-php' => 
+    array (
+      'pretty_version' => '0.2.4',
+      'version' => '0.2.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e48e090d6049c3e6a4f3684b4893762e80d54c1e',
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '408d5eafb83c57f6365a3ca330ff23aa4a5fa39b',
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+  ),
+);
